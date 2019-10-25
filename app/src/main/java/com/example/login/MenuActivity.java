136720c8fbfefdd417671848b86f04ffeb99c1da
Yorvin Palacios Gomez <yorvin_20_1997@hotmail.com>
2019-10-24 21:19:12 -0500

    actualizado

--- app/src/main/java/com/example/login/MenuActivity.java
+++ app/src/main/java/com/example/login/MenuActivity.java
@@ -3,12 +3,19 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MenuActivity extends AppCompatActivity {
+
+    TextView txtnom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        txtnom = findViewById(R.id.txtnom);
     }
 }
